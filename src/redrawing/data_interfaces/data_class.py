26c6e842b2f71bda9d1f3f6b922ce4ab50fbbe10
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-04-01 11:20:06 -0300

    Documentação da classe Data
    Classe Data documentada. Método de codificação nomeado corretamente (test -> encoder)

--- src/redrawing/data_interfaces/data_class.py
+++ src/redrawing/data_interfaces/data_class.py
@@ -1,21 +1,25 @@
+from abc import ABC
 import json
 from json import JSONEncoder
 import time
 
 import numpy as np
 
-#https://pynative.com/python-serialize-numpy-ndarray-into-json/
-class NumpyArrayEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
 
 
-def test(obj):
+def encoder(obj):
+    '''!
+        Encode a Data class to JSON
+
+        Implements encoding of numpy data types. Other data types are returned as dicts
+
+        Parameters:
+            obj (Object): object to be encoded
+        
+        Returns
+            @returns encoded object
+    '''
+
     if isinstance(obj, np.integer):
             return int(obj)
     elif isinstance(obj, np.floating):
@@ -25,13 +29,25 @@ def test(obj):
     
     return obj.__dict__
 
-class Data:
+class Data(ABC):
+    '''!
+        Base class for data messages classes
+    '''
+
     def __init__(self):
         pass
 
     def toJSON(self):
+        '''!
+            Convert object to JSON
+        '''
+
         self.__time = time.time()
-        return json.dumps(self, default=lambda o: test(o))
+        return json.dumps(self, default=lambda o: encoder(o))
     
     def toMessage(self):
+        '''!
+            Converct object to JSON encoded ready to send message
+        '''
+
         return self.toJSON().encode("utf-8")

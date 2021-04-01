1e6b283bdb7c791efe275adca3d114e667ec7376
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-04-01 11:37:57 -0300

    Documentação de udp and image files
    Documentação da função para envio de mensagens via UDP e classe de dados para Imagens

--- src/redrawing/communication/udp.py
+++ src/redrawing/communication/udp.py
@@ -3,6 +3,15 @@ import time
 from redrawing.data_interfaces.data_class import Data
 
 def send_data(data):
+    '''!
+        Sends the message by UDP
+
+        Parameters:
+            @param data (data_interfaces.Data): the data object that will be sent
+
+        @todo udp.py - Implementar classe adequada: ela deve possuir capacidade de alterar endereço ip e porta de envio. Singleton?
+    '''
+
     if not isinstance(data, Data):
         raise Exception()
     
--- src/redrawing/data_interfaces/image.py
+++ src/redrawing/data_interfaces/image.py
@@ -3,16 +3,50 @@ import numpy as np
 from redrawing.data_interfaces.data_class import Data
 
 class Image(Data):
+    '''!
+        Data class for image messages
+    '''
+
     def __init__(self, frame_id = "UNKNOWN"):
+        '''!
+            Image constructor
+
+            Parameters:
+                @param frame_id (string): the frame where the camera that captured the image is
+        '''
+
         self.__frame_id = frame_id
         self.__image = np.asarray([],dtype=np.uint8)
     
     def set_image(self, image):
+        '''!
+            Set the image data
+
+            Parameters:
+                @param image: the image data. Must be a numpy.array or a list
+
+        '''
+
         if isinstance(image, np.ndarray):
             self.__image = image.astype(np.uint8)
+        else:
+            self.__image = image.array(image, dtype=np.uint8)
     
     def get_image(self):
+        '''!
+            Gets the image data
+
+            Returns
+                @return image (numpy.array): the image data
+        '''
+
         return self.__image
 
     def get_frame_id(self):
+        '''!
+            Gets the frame id
+
+            Returns
+                @return frame_id (string): the frame where the camera that captured the image is
+        '''
         return self.__frame_id
